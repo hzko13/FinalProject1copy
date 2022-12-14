f606b1dba88b19326906ba88158674972298da89
ibtehaj01 <73610607+ibtehaj01@users.noreply.github.com>
2022-12-13 21:12:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -81,11 +81,5 @@
   *  Event listener for HTTP server "listening" event
   */
  
- function onListening() {
-   var addr = server.address();
-   var bind = typeof addr === 'string'
-     ? 'pipe ' + addr
-     : 'port ' + addr.port;
-   debug('Listening on ' + bind);
- }
+
  
